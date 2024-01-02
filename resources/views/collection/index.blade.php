83848283bdd3542a7d6d44425bb0d70f860dbe4d
tim <timdesmet00@gmail.com>
2024-01-02 13:55:53 +0100

    debut sass

--- resources/views/collection/index.blade.php
+++ resources/views/collection/index.blade.php
@@ -3,28 +3,29 @@
 @section('content')
 
 <h2>Acceuil</h2>
-
-@foreach ($collections as $item)
-    <div class="card">
-        <h3>{{ $item->shortname }}</h3>
-        <!-- <p>{{ $item->fullname }}</p> -->
-        <img src="{{ $item->image }}" alt="{{ $item->shortname }}">
-        <p> Genre: {{ $item->firstgender }}  {{ $item->secondgender }}  {{ $item->thirdgender }}</p>
-        <!-- <p>{{ $item->secondgender }}</p> -->
-        <!-- <p>{{ $item->thirdgender }}</p> -->
-        <p>Sortie en: {{ $item->year }}</p>
-        <!-- <p>{{ $item->description }}</p> -->
-        <div class="link">
-            <button class="btn" onclick="location.href='{{ $item->link }}'">Info</button>
-            <button class="btn" onclick="location.href='{{ route('collection.edit', $item->id) }}'">Modifier</button>
-            <button class="btn" onclick="location.href='{{ route('collection.show', $item->id) }}'">Afficher</button>
-            <form action="{{ route('collection.destroy', $item->id) }}" method="post">
-                @csrf
-                @method('DELETE')
-                <input type="submit" value="Supprimer">
-            </form>
+<section class="card-container">
+    @foreach ($collections as $item)
+        <div class="card">
+            <h3>{{ $item->shortname }}</h3>
+            <div class="img">
+                <img src="{{ asset('img/' . $item->image) }}" alt="{{ $item->shortname }}">
+            </div>
+            <p> Genre: {{ $item->firstgender }} 
+                @if(!empty($item->secondgender)) / {{ $item->secondgender }} @endif
+                @if(!empty($item->thirdgender)) / {{ $item->thirdgender }} @endif
+            </p>
+            <p>Sortie en: {{ $item->year }}</p>
+            <div class="link">
+                <button class="btn" onclick="location.href='{{ $item->link }}'">Info</button>
+                <button class="btn" onclick="location.href='{{ route('collection.edit', $item->id) }}'">Modifier</button>
+                <button class="btn" onclick="location.href='{{ route('collection.show', $item->id) }}'">Afficher</button>
+                <form action="{{ route('collection.destroy', $item->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="Supprimer">
+                </form>
+            </div>
         </div>
-    </div>
-@endforeach
-
+    @endforeach
+</section>
 @endsection

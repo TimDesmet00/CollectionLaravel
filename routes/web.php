13b2c72bf1122d9e0706cd8e4e01f314948cb96f
tim <timdesmet00@gmail.com>
2024-01-05 08:40:20 +0100

    verif de la fin de journée d'hier

--- routes/web.php
+++ routes/web.php
@@ -23,20 +23,20 @@ use App\Http\Controllers\UserController;
 Route::get('/', [CollectionController::class, 'index'])->name('collection.index');
 Route::prefix('collection')->group(function () {
     
-    Route::get('create', [CollectionController::class, 'create'])->name('collection.create')->middleware('auth');
-    Route::post('create', [CollectionController::class, 'store'])->name('collection.store')->middleware('auth');
+    Route::get('create', [CollectionController::class, 'create'])->name('collection.create')->middleware(['auth', 'verified']);
+    Route::post('create', [CollectionController::class, 'store'])->name('collection.store')->middleware(['auth', 'verified']);
     Route::get('{collection}', [CollectionController::class, 'show'])->name('collection.show');
-    Route::get('{collection}/edit', [CollectionController::class, 'edit'])->name('collection.edit')->middleware('auth');
-    Route::put('{collection}', [CollectionController::class, 'update'])->name('collection.update')->middleware('auth');
-    Route::delete('{collection}', [CollectionController::class, 'destroy'])->name('collection.destroy')->middleware('auth');
+    Route::get('{collection}/edit', [CollectionController::class, 'edit'])->name('collection.edit')->middleware(['auth', 'verified']);
+    Route::put('{collection}', [CollectionController::class, 'update'])->name('collection.update')->middleware(['auth', 'verified']);
+    Route::delete('{collection}', [CollectionController::class, 'destroy'])->name('collection.destroy')->middleware(['auth', 'verified']);
 });
 
 Route::prefix('user')->group(function () {
     Route::get('create', [UserController::class, 'create'])->name('user.create');
     Route::post('create', [UserController::class, 'store'])->name('user.store');
-    Route::get('{user}', [UserController::class, 'edit'])->name('user.edit')->middleware('auth');
-    Route::put('{user}', [UserController::class, 'update'])->name('user.update')->middleware('auth');
-    Route::delete('{user}', [UserController::class, 'destroy'])->name('user.destroy')->middleware('auth');
+    Route::get('{user}', [UserController::class, 'edit'])->name('user.edit')->middleware(['auth', 'verified']);
+    Route::put('{user}', [UserController::class, 'update'])->name('user.update')->middleware(['auth', 'verified']);
+    Route::delete('{user}', [UserController::class, 'destroy'])->name('user.destroy')->middleware(['auth', 'verified']);
 });
 
 Route::get('login', [UserController::class, 'loginForm'])->name('user.login');

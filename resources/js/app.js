2c97926b9606311cb248d22c17259cdb4a58f3eb
tim <timdesmet00@gmail.com>
2024-01-12 11:30:52 +0100

    toggle color du svg favorite

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,20 @@ import "./bootstrap";
 import { addGenre } from "./components/btnGender";
 import { addFavorite } from "./components/favorite";
 
-addGenre();
-addFavorite();
+try {
+    addGenre();
+} catch (error) {
+    console.error(
+        "Une erreur s'est produite lors de l'exécution de addGenre:",
+        error
+    );
+}
+
+try {
+    addFavorite();
+} catch (error) {
+    console.error(
+        "Une erreur s'est produite lors de l'exécution de addFavorite:",
+        error
+    );
+}
--- resources/js/components/favorite.js
+++ resources/js/components/favorite.js
@@ -1,10 +1,33 @@
+// export function addFavorite() {
+//     document.addEventListener("DOMContentLoaded", (event) => {
+//         const svgElements = document.querySelectorAll(".add-favorite");
+//         svgElements.forEach((svgElement) => {
+//             svgElement.addEventListener("click", () => {
+//                 if (svgElement.style.fill === "#ffd700") {
+//                     svgElement.style.fill = "#97AABD"; // Remplacez par la couleur de base
+//                 } else {
+//                     svgElement.style.fill = "#ffd700";
+//                 }
+//                 let toggleEvent = new ToggleEvent();
+//             });
+//         });
+//     });
+// }
+
 export function addFavorite() {
     document.addEventListener("DOMContentLoaded", (event) => {
         const svgElements = document.querySelectorAll(".add-favorite");
         svgElements.forEach((svgElement) => {
             svgElement.addEventListener("click", () => {
-                svgElement.style.fill = "#ff0000";
-                ToggleEvent();
+                let style = window.getComputedStyle(svgElement);
+                let fill = style.getPropertyValue("fill");
+                if (fill === "rgb(255, 215, 0)") {
+                    // #ffd700 en RGB
+                    svgElement.setAttribute("fill", "#97AABD"); // Remplacez par la couleur de base
+                } else {
+                    svgElement.setAttribute("fill", "#ffd700");
+                }
+                let toggleEvent = new ToggleEvent();
             });
         });
     });
